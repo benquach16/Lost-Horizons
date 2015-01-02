3f7f9055ecaf033aa5518a2f99e2f24f7e82b214
Ben Quach <benquach16@yahoo.com>
2015-01-02 02:19:17 -0800

    setting up some frameworks real quick

---
+++ losthorizons/prerenderevents.h
@@ -0,0 +1,17 @@
+#ifndef __PRERENDEREVENTS_H__
+#define __PRERENDEREVENTS_H__
+
+#include <irrlicht.h>
+
+//needed so we can to render to texture properly
+class PrerenderEvents
+{
+public:
+	PrerenderEvents();
+	~PrerenderEvents();
+
+protected:
+
+};
+
+#endif
