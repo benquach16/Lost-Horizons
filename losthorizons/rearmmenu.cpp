29ddca0573f077d980d742c23e2d93f8ebef9d9e
Ben Quach <benquach16@yahoo.com>
2015-09-22 13:05:46 -0700

    Implementing slot system

--- losthorizons/rearmmenu.cpp
+++ losthorizons/rearmmenu.cpp
@@ -13,21 +13,52 @@ using namespace scene;
 const vector3df cameraPosition = vector3df(0,200,0);
 const video::SColor renderColor = video::SColor(128,100,120,140);
 
-RearmMenu::RearmMenu()
+WeaponSlot::WeaponSlot(E_TURRET_CLASS type, irr::core::vector2di position) :
+	type(type), position(position)
+{
+}
+
+void WeaponSlot::draw()
+{
+	//draw stuff
+	//inc bad code
+	if(type == TURRET_HEAVY)
+	{
+		vdriver->draw2DImage(vdriver->getTexture("res/menu/heavy_slot.png"), position, rect<s32>(0,0,32,32), 0, video::SColor(255,255,255,255), true);
+	}
+	else if(type == TURRET_MEDIUM)
+	{
+		vdriver->draw2DImage(vdriver->getTexture("res/menu/medium_slot.png"), position, rect<s32>(0,0,32,32), 0, video::SColor(255,255,255,255), true);
+	}
+	else if(type == TURRET_LIGHT)
+	{
+		vdriver->draw2DImage(vdriver->getTexture("res/menu/light_slot.png"), position, rect<s32>(0,0,32,32), 0, video::SColor(255,255,255,255), true);
+	}
+
+}
+
+bool WeaponSlot::getWithinBoundingBox()
+{
+	int x = receiver->getMouseX();
+	int y = receiver->getMouseY();
+	//make sure the mouse is within the slot
+	return ((x > position.X - 16 ) && (x < position.X + 16) && (y > position.Y - 16) && (y < position.Y + 16));
+}
+
+
+RearmMenu::RearmMenu() : currentSelected(-1)
 {
 	//make important stuff
 	//do render to texture
 	window = guienv->addWindow(rect<s32>(width/2-400,height/2-300,width/2+400,height/2+300), false, L"Rearm");
-	shipsInFleet = guienv->addListBox(rect<s32>(10,40,60,600),window);
+	availableWeapons = guienv->addListBox(rect<s32>(10,410,100,600),window);
+	shipsInFleet = guienv->addListBox(rect<s32>(10,40,60,400),window);
 	shipImage = guienv->addImage(rect<s32>(70,20,600,400), window,-1,L"",false);
 	//render to texture 
 	rt = vdriver->addRenderTargetTexture(core::dimension2d<u32>(base::width/2,base::height/2), "ShipRTT");
 	shipCamera = scenemngr->addCameraSceneNode(0,cameraPosition,vector3df(0,0,0),-1,false);
 
 	
-	//set the texture to a something	
-	
-
 }
 
 RearmMenu::~RearmMenu()
@@ -44,19 +75,45 @@ void RearmMenu::loadFleet(const Fleet* fleet)
 	}
 }
 
+void RearmMenu::reloadShip(Ship *ship)
+{
+	weaponImages.clear();
+	for(unsigned i = 0; i < ship->getTurrets(TURRET_HEAVY).size(); i++)
+	{
+		vector2di t = scenemngr->getSceneCollisionManager()->getScreenCoordinatesFrom3DPosition(ship->getTurrets(TURRET_MEDIUM)[i]->getPosition());
+		t.X -= 16;
+		t.Y -= 16;
+		weaponImages.push_back(WeaponSlot(TURRET_HEAVY,t));
+	}
+	for(unsigned i = 0; i < ship->getTurrets(TURRET_MEDIUM).size(); i++)
+	{
+		vector2di t = scenemngr->getSceneCollisionManager()->getScreenCoordinatesFrom3DPosition(ship->getTurrets(TURRET_MEDIUM)[i]->getPosition());
+		t.X -= 16;
+		t.Y -= 16;
+		weaponImages.push_back(WeaponSlot(TURRET_MEDIUM,t));
+	}
+	for(unsigned i = 0; i < ship->getTurrets(TURRET_LIGHT).size(); i++)
+	{
+		vector2di t = scenemngr->getSceneCollisionManager()->getScreenCoordinatesFrom3DPosition(ship->getTurrets(TURRET_LIGHT)[i]->getPosition());
+		t.X -= 16;
+		t.Y -= 16;
+		weaponImages.push_back(WeaponSlot(TURRET_LIGHT,t));
+	}
+}
+
 void RearmMenu::run()
 {
+	//make sure we stop running once the window is closed
 	
 	Ship *player = Ship::allShips[0];
-	
 	//this is gunna be a right bitch
 	//temporarily swap the cameras
 	//THIS MUST BE RUN BEFORE POSTPROCESSING CALLS
 	
-	if(rt)
+	if(rt && (shipsInFleet->getSelected() != -1))
 	{
 		//how do we run this before the render calls?
-		
+
 		ICameraSceneNode *cam = scenemngr->getActiveCamera();
 		vdriver->setRenderTarget(rt, true, true, renderColor);
 
@@ -69,37 +126,28 @@ void RearmMenu::run()
 		//Ship::allShips[0]->getMesh()->render();
 		scenemngr->getActiveCamera()->render();
 		Ship::allShips[0]->getMesh()->render();
+
+		//swap code
+		if(currentSelected != shipsInFleet->getSelected())
+		{
+			currentSelected = shipsInFleet->getSelected();
+			reloadShip(Ship::allShips[0]);
+		}
 		//draw boxes aroundthe ship turret slots
 		// set back old render target
 		// The buffer might have been distorted, so clear it
-		//TODO: get rid of vector copying here
-		//std::vector<vector2d<s32> > medTurrets = player->getMediumTurretEquipPosition();
-		//offset the icon by the center of the picture
-		for(unsigned i = 0; i < player->getTurrets(TURRET_HEAVY).size(); i++)
+		for(int i = 0; i < weaponImages.size(); i++)
 		{
-			//player->getTurrets(TURRET_MEDIUM)[i]->renderPosition();
-			vector2di t = scenemngr->getSceneCollisionManager()->getScreenCoordinatesFrom3DPosition(player->getTurrets(TURRET_HEAVY)[i]->getPosition());
-			t.X -= 16;
-			t.Y -= 16;
-			vdriver->draw2DImage(vdriver->getTexture("res/menu/heavy_slot.png"), t, rect<s32>(0,0,32,32), 0, video::SColor(255,255,255,255), true);
-		}
-		for(unsigned i = 0; i < player->getTurrets(TURRET_MEDIUM).size(); i++)
-		{
-			//player->getTurrets(TURRET_MEDIUM)[i]->renderPosition();
-			vector2di t = scenemngr->getSceneCollisionManager()->getScreenCoordinatesFrom3DPosition(player->getTurrets(TURRET_MEDIUM)[i]->getPosition());
-			t.X -= 16;
-			t.Y -= 16;
-			vdriver->draw2DImage(vdriver->getTexture("res/menu/medium_slot.png"), t, rect<s32>(0,0,32,32), 0, video::SColor(255,255,255,255), true);
-		}
-		for(unsigned i = 0; i < player->getTurrets(TURRET_LIGHT).size(); i++)
-		{
-			//player->getTurrets(TURRET_MEDIUM)[i]->renderPosition();
-			vector2di t = scenemngr->getSceneCollisionManager()->getScreenCoordinatesFrom3DPosition(player->getTurrets(TURRET_LIGHT)[i]->getPosition());
-			t.X -= 16;
-			t.Y -= 16;
-			vdriver->draw2DImage(vdriver->getTexture("res/menu/light_slot.png"), t, rect<s32>(0,0,32,32), 0, video::SColor(255,255,255,255), true);
+			weaponImages[i].draw();
+			if(weaponImages[i].getWithinBoundingBox() && (receiver->isKeyPressed(irr::KEY_LBUTTON)))
+			{
+				//pressed a button
+				//load the weapon
+				std::cout << "Icon clicked!" << std::endl;
+			}
 		}
 
+		std::vector<ObjectManager::E_ITEM_LIST> weaponsList = player->getInventory().getMediumWeapons();
 		vdriver->setRenderTarget(0, true, true, 0);
 		//could we draw the render here?
 
--- losthorizons/rearmmenu.h
+++ losthorizons/rearmmenu.h
@@ -3,6 +3,20 @@
 
 #include <irrlicht.h>
 #include "fleet.h"
+#include "turretproperties.h"
+
+//we're going to use a slots system to let players choose what weapon goes in whichever slot
+class WeaponSlot
+{
+public:
+	WeaponSlot(E_TURRET_CLASS type, irr::core::vector2di position);
+	void draw();
+	bool getWithinBoundingBox();
+protected:
+	irr::core::stringc texture;
+	irr::core::vector2di position;
+	E_TURRET_CLASS type;
+};
 
 //because we need to make equipping fleet ships more intuitive
 class RearmMenu
@@ -12,8 +26,11 @@ public:
 	~RearmMenu();
 
 	void loadFleet(const Fleet* fleet);
+	void reloadShip(Ship *ship);
 	void run();
+	void equipWeapons();
 protected:
+	int currentSelected;
 	irr::gui::IGUIWindow *window;
 	irr::gui::IGUIListBox *shipsInFleet;
 	irr::gui::IGUIImage *shipImage;
@@ -23,6 +40,7 @@ protected:
 	//irr::gui::IGUiTreeView *equipmentTree;
 	irr::gui::IGUIListBox *availableWeapons;
 	irr::gui::IGUIStaticText *description;
+	std::vector<WeaponSlot> weaponImages;
 };
 
 #endif
